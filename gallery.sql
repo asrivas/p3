aa6d7b5c593342c9c5aee47d083eebbd99aa40f5
asrivas <asrivastava274@gmail.com>
2013-10-06 18:16:23 -0700

    added all init sql data

--- gallery.sql
+++ gallery.sql
@@ -40,17 +40,41 @@ INSERT INTO users (name, password, fact1, fact2, fact3)
 VALUES ('User4', 'password1', 'fact1', 'fact2', 'fact3');
 
 INSERT INTO albums (user_id, album_name)
-VALUES (1, 'Album1');
+VALUES (1, 'Avocados');
 INSERT INTO albums (user_id, album_name)
-VALUES (2, 'Jasons Album');
+VALUES (2, 'Amy Winehouse');
 INSERT INTO albums (user_id, album_name)
-VALUES (3, 'Album3');
+VALUES (3, 'Berries');
 INSERT INTO albums (user_id, album_name)
-VALUES (4, 'Album4');
+VALUES (4, 'Shoes');
 INSERT INTO albums (user_id, album_name)
 VALUES (4, 'Album5');
 
 INSERT INTO photos(album_id, user_id, title, file)
-VALUES(1, 1, 'San Francisco', 'sf1.jpg');
+VALUES(1, 1, 'Avocado1', 'av1.jpg');
 INSERT INTO photos(album_id, user_id, title, file)
-VALUES(1, 1, 'San Francisco', 'sf2.jpg');
+VALUES(1, 1, 'Avocado2', 'av2.jpg');
+INSERT INTO photos(album_id, user_id, title, file)
+VALUES(1, 1, 'Avocado3', 'av3.jpg');
+
+INSERT INTO photos(album_id, user_id, title, file)
+VALUES(2, 2, 'Amy1', 'amy1.jpg');
+INSERT INTO photos(album_id, user_id, title, file)
+VALUES(2, 2, 'Amy2', 'amy2.jpg');
+INSERT INTO photos(album_id, user_id, title, file)
+VALUES(2, 2, 'Amy3', 'amy3.jpg');
+
+INSERT INTO photos(album_id, user_id, title, file)
+VALUES(3, 3, 'Berries1', 'berries1.jpg');
+INSERT INTO photos(album_id, user_id, title, file)
+VALUES(3, 3, 'Berries2', 'berries2.jpg');
+INSERT INTO photos(album_id, user_id, title, file)
+VALUES(3, 3, 'Berries3', 'berries3.jpg');
+
+INSERT INTO photos(album_id, user_id, title, file)
+VALUES(4, 4, 'Shoes1', 'shoes1.jpg');
+INSERT INTO photos(album_id, user_id, title, file)
+VALUES(4, 4, 'Shoes2', 'shoes2.jpg');
+INSERT INTO photos(album_id, user_id, title, file)
+VALUES(4, 4, 'Shoes3', 'shoes3.jpg');
+
